94bad0d425c55381acd1418a317e38483ef6a63c
Sasha <dresvannikovaa1@gmail.com>
2026-05-16 20:58:06 +0300

    Фиксы HomePage пробуем

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture()
 def main_tab():
     with sync_playwright() as ad:
-        browser = ad.chromium.launch(headless=False)
+        browser = ad.chromium.launch(headless=True)
         tab = browser.new_page()
         tab.goto("https://the-internet.herokuapp.com/")
         tab.wait_for_load_state('networkidle')
@@ -15,7 +15,7 @@ def main_tab():
 @pytest.fixture()
 def geo():
     with sync_playwright() as ad:
-        browser = ad.chromium.launch(headless=False)
+        browser = ad.chromium.launch(headless=True)
         context = browser.new_context(
             permissions=['geolocation'],
             geolocation={'latitude': 40.712776, 'longitude': -74.005974}
--- pages/home_page.py
+++ pages/home_page.py
@@ -1,14 +1,21 @@
-def go(self, link_text: str):
-    link = self.page.locator(f"//a[text()='{link_text}']")
-    self.highlight(link, 'red', 'lightcoral')
-    assert link.text_content() == link_text
-    link.click()
-    self.page.wait_for_load_state('networkidle')
-    self.unhighlight(link)
 
-def go_to( link_text, main_tab):
-    link = main_tab.locator(f"//a[text()='{link_text}']")
-    assert link.text_content() == link_text
-    link.click()
-    main_tab.wait_for_load_state('networkidle')
+from helpers.visual_helpers import color_element, closing_element
+
+class HomePage:
+    def __init__(self, page):
+        self.page = page
+
+    def go(self, link_text: str):
+        link = self.page.locator(f"//a[text()='{link_text}']")
+        color_element(link, 'red', 'lightcoral')
+        assert link.text_content() == link_text
+        link.click()
+        self.page.wait_for_load_state('networkidle')
+
+
+    def go_to(link_text, main_tab):
+        link = main_tab.locator(f"//a[text()='{link_text}']")
+        assert link.text_content() == link_text
+        link.click()
+        main_tab.wait_for_load_state('networkidle')
 
